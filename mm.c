856eb216fec45f6b4409ba837f93de2202fa420b
Coffeetea <coffeetea99iro@gmail.com>
2019-11-08 08:28:00 +0900

    made basic functions

--- mm.c
+++ mm.c
@@ -41,6 +41,20 @@ static range_t **gl_ranges;
 
 /* user-defined functions */
 
+/* get the size of allocated memory
+ * return the segregated list index of it
+ */
+static int size_to_index(int size) {
+    int block = ALIGN(size) >> 3;
+    int index = 0;
+    int cur = 1;
+    while( cur < block ) {
+        cur *= 2;
+        ++index;
+    }
+    return index;
+}
+
 /* get start pointer of the block
  * return pointer of its header
  */
@@ -51,14 +65,14 @@ static void* header_pointer(void* start) {
 /* get start pointer of the block
  * return its header entry(4 byte)
  */
-static int header(void* start) {
-    return *((int*)header_pointer(start));
+static size_t header(void* start) {
+    return *((size_t*)header_pointer(start));
 }
 
 /* get start pointer of the block
  * return the block's size(4 byte)
  */
-static int block_size(void* start) {
+static size_t block_size(void* start) {
     return header(start) & ~0x7;
 }
 
@@ -66,14 +80,14 @@ static int block_size(void* start) {
  * return pointer of its footer
  */
 static void* footer_pointer(void* start) {
-    return (char*)start + block_size(start);
+    return (char*)start + block_size(start) + 8;
 }
 
 /* get start pointer of the block
  * return its footer entry(4 byte) 
  */
-static int footer(void* start) {
-    return *((int*)footer_pointer(start));
+static size_t footer(void* start) {
+    return *((size_t*)footer_pointer(start));
 }
 
 /* get start pointer of the block
@@ -86,41 +100,83 @@ static char is_allocated(void* start) {
 /* get start pointer of the block and block size that will change into
  * rewrite size of the block on header and footer
  */
-static void reset_block_size(void* start, int size){
-    int* header_pointer_4bit = header_pointer(start);
+static void reset_block_size(void* start, size_t size){
+    size_t* header_pointer_4bit = header_pointer(start);
     *header_pointer_4bit = size | (*header_pointer_4bit & 0x1);
-    int* footer_pointer_4bit = footer_pointer(start);
+    size_t* footer_pointer_4bit = footer_pointer(start);
     *footer_pointer_4bit = *header_pointer_4bit;
 }
 
 /* get start pointer of the block and block status that will change into
  * rewrite status of the block on header and footer
  */
-static void rewrite_block_status(void* start, int status) {
-    int* header_pointer_4bit = header_pointer(start);
-    int* footer_pointer_4bit = footer_pointer(start);
+static void rewrite_block_status(void* start, char status) {
+    size_t* header_pointer_4bit = header_pointer(start);
+    size_t* footer_pointer_4bit = footer_pointer(start);
     *header_pointer_4bit = status | (*header_pointer_4bit & ~0x7);
     *footer_pointer_4bit = status | (*footer_pointer_4bit & ~0x7);
 }
 
-/* get start pointer of the block
+/* get start pointer of a free block
+ * cut off the block from segregated list
+ */
+static void cut_off(void* start) {
+    int left_offset = *(int*)start;
+    int right_offset = *(int*)((char*)start + 4);
+    if ( left_offset ) {
+        char* left_pointer = (char*)start + left_offset;
+        *(int*)(left_pointer + 4) += right_offset;
+    }
+    if ( right_offset ) {
+        char* right_pointer = (char*)start + right_offset;
+        *(int*)right_pointer += left_offset;
+    }
+}
+
+/* get start pointer and size of block
+ * put the block in the segregated list
+ */
+static void set_in(void* start, int size) {
+    int index = size_to_index(size);
+    int* array = mem_heap_lo();
+    int offset = array[index];
+
+    char* pointer = (char*)mem_heap_lo() + 128 + offset;
+    while( offset = *(int*)(pointer+4) ) {
+        pointer += offset;
+    }
+
+    int gap = (char*)start - pointer;
+    *(int*)(pointer + 4) = gap;
+    *(int*)start = -gap;
+    *(int*)((char*)start + 4) = 0;
+}
+
+/* get start pointer of a free block
  * if can coalesce with front block, coalesce and return new pointer
  * if not, just return original pointer
  */
 static void* coalesce_front(void* start) {
-    int* front_footer_pointer = (char*)start - 8;
-    int front_footer = *front_footer_pointer;
+    size_t* front_footer_pointer = (char*)start - 8;
+    size_t front_footer = *front_footer_pointer;
     char front_allocated = (char)front_footer & 0x1;
-    if ( front_allocated != is_allocated(start) ) {
+    if ( front_allocated ) {    //can't coaleace
         return start;
-    } else {
-        int front_size = front_footer & ~0x7;
-        int total_size = front_size + block_size(start) + 8;
+    } else {                    //can coaleace
+        size_t front_size = front_footer & ~0x7;
+        size_t total_size = front_size + block_size(start) + 16;
+        void* front_pointer = (char*)start - front_size - 16;
 
-        void* front_pointer = (char*)start - front_size - 8;
+        cut_off(start);
+        cut_off(front_pointer);
+
         int* front_header_pointer_4bit = (char*)front_pointer - 4;
         int* footer_pointer_4bit = footer_pointer(start);
-        *front_header_pointer_4bit = *footer_pointer_4bit = total_size | front_allocated;
+        *front_header_pointer_4bit = total_size;
+        *footer_pointer_4bit = total_size;
+
+        set_in(front_pointer, total_size);
+
         return front_pointer;
     }
 }
@@ -130,17 +186,23 @@ static void* coalesce_front(void* start) {
  * return nothing
  */
 static void coalesce_back(void* start) {
-    int* back_header_pointer = (char*)start + block_size(start) + 4;
+    int* back_header_pointer = (char*)start + block_size(start) + 12;
     int back_header = *back_header_pointer;
     char back_allocated = (char)back_header & 0x1;
-    if ( back_allocated == is_allocated(start)) {
+    if ( !back_allocated ) {
         int back_size = back_header & ~0x7;
-        int total_size = back_size + block_size(start) + 8;
+        int total_size = back_size + block_size(start) + 16;
 
-        int* back_footer_pointer_4bit = (char*)start + total_size;
+        void* back_pointer = (char*)start + block_size(start) + 16;
+        cut_off(start);
+        cut_off(back_pointer);
+
+        int* back_footer_pointer_4bit = (char*)start + total_size + 8;
         int* header_pointer_4bit = header_pointer(start);
-        *header_pointer_4bit = total_size | back_allocated;
-        *back_footer_pointer_4bit = total_size | back_allocated;
+        *header_pointer_4bit = total_size;
+        *back_footer_pointer_4bit = total_size;
+
+        set_in(start, total_size);
     }
 }
 
